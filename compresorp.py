28c0de7150f6291897404ae346b7ee3571453a30
dgop92 <inevaup@gmail.com>
2023-05-05 17:36:01 -0500

    feat: add compression in parallel

---
+++ compresorp.py
@@ -0,0 +1,70 @@
+import argparse
+import logging
+import pickle
+import time
+
+from mpi4py import MPI
+
+from config.logging import config_logger
+from core.dahuffman import HuffmanCodec
+
+if __name__ == "__main__":
+    config_logger()
+    parser = argparse.ArgumentParser(description="A compressor utility")
+    parser.add_argument("input_file", type=str)
+    parser.add_argument("output_file", type=str, nargs="?")
+    args = parser.parse_args()
+
+    start = 0
+    end = 0
+
+    if args.output_file is None:
+        args.output_file = "comprimido.elmejorprofesor"
+
+    text_file = args.input_file
+    output_file = args.output_file
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    if rank == 0:
+        start = time.time()
+        logging.info(f"Reading {text_file}")
+        with open(text_file, "rb") as f:
+            content = f.read()
+
+        # split content
+        total_length = len(content)
+        split_length = total_length // size
+        splits = [
+            content[i : i + split_length] for i in range(0, total_length, split_length)
+        ]
+        if len(splits) > size:
+            last_split = splits.pop()
+            splits[-1] += last_split
+
+        codec = HuffmanCodec.from_data(content)
+        content_with_codec = [(codec, split) for split in splits]
+    else:
+        content_with_codec = None
+
+    content_with_codec = comm.scatter(content_with_codec, root=0)
+
+    logging.info(f"Compressing {rank}")
+    codec, content = content_with_codec
+    compressed = codec.encode(content)
+
+    compressed_content = comm.gather(compressed, root=0)
+
+    if rank == 0:
+        logging.info(f"Writing {output_file}")
+        with open(output_file, "wb") as f:
+            compress_data = {
+                "codec_data": codec.get_codec_data(),
+                "encoded": compressed_content,
+            }
+            pickle.dump(compress_data, f)
+
+        end = time.time()
+        print(f"Compressing {text_file} took {end - start} seconds")
--- core/decompress.py
+++ core/decompress.py
@@ -1,6 +1,8 @@
 import logging
 import pickle
 import time
+from multiprocessing import Pool
+from typing import List
 
 from core.dahuffman import HuffmanCodec
 
@@ -17,6 +19,43 @@ def decompress_file(compressed_file: str, output_file: str):
         with open(output_file, "wb") as file:
             logging.info(f"Writing to {output_file}")
             file.write(decoded)
+
+
+def decompress_content_in_parallel(
+    compressed_content: List[bytes],
+    codec: HuffmanCodec,
+    n_cores=2,
+) -> str:
+    with Pool(processes=n_cores) as pool:
+        results = pool.map(codec.decode, compressed_content)
+
+    return b"".join(results)
+
+
+def decompress_file_in_parallel(compressed_file: str, output_file: str, n_cores=2):
+    logging.info(f"Reading {compressed_file}")
+    with open(compressed_file, "rb") as f:
+        compress_data = pickle.load(f)
+
+    codec = HuffmanCodec.create_from_data(compress_data["codec_data"])
+
+    logging.info(f"Decompressing {compressed_file} in parallel")
+    decompressed_content = decompress_content_in_parallel(
+        compress_data["encoded"], codec, n_cores
+    )
+
+    logging.info(f"Writing to {output_file}")
+    with open(output_file, "wb") as f:
+        f.write(decompressed_content)
+
+
+def decompress_file_in_parallel_logging_time(
+    compressed_file: str, output_file: str, n_cores=2
+):
+    start = time.time()
+    decompress_file_in_parallel(compressed_file, output_file, n_cores)
+    end = time.time()
+    print(f"Decompressing {compressed_file} took {end - start} seconds")
 
 
 def decompress_file_logging_time(compressed_file: str, output_file: str):
