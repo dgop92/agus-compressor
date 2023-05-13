6aa2233f18044ae6136bd84592207cf9d3eeb7db
Jaider Olivella <78622710+Derjai@users.noreply.github.com>
2023-05-13 11:36:55 -0500

    Updated balancer.py
    Fix: Guarantees that the distribution output is done in order when n>p

--- core/balancer.py
+++ core/balancer.py
@@ -7,9 +7,15 @@ def distribute(parts, processes):
         allocations = [[part] for part in parts]
     elif n > p:
         # Distribute between processes
-        allocations = [[] for _ in range(p)]
-        for i, part in enumerate(parts):
-            allocations[i % p].append(part)
+        parts_per_process= n//p
+        module = n%p
+        allocations = []
+        init = 0
+        for i in range (p):
+            end = init + parts_per_process + (1 if i<module else 0)
+            allocations.append(parts[init:end])
+            init=end
+        return allocations 
     else:
         # Distribute between processes, regardless of processes having no part assigned to it
         allocations = [[] for _ in range(p)]
